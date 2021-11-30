d90669060762e0e297ee48bca767508f84ba6e74
Ex1Kx <thenewestekigoh@gmail.com>
2021-11-30 11:30:26 -0500

    [Ex1] Added The Files

---
+++ factor/factor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace factor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fac = 1, x, n;
+            string linea;
+            x = 1;
+            Console.Write("Ingrese un numero entero: ");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            while (x <= n)
+            {
+                fac = fac * x;
+                x = x + 1;
+            }
+            Console.Write("La factorial es: " + fac);
+            Console.ReadKey();
+        }
+    }
+}
